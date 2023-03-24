d5f3bcf930cdcced002712db5f9843be8e0aaa3a
Chukwuezi Benjamin Chimaobi <benjaminchima072@gmail.com>
2023-03-24 22:24:28 +0000

    added the remaining visa types pages

---
+++ scripts/visa4.js
@@ -0,0 +1,46 @@
+function query(a){
+    return document.querySelector(a);
+}
+
+
+function listener(funcName,funcType,funcElement){
+    funcElement.addEventListener(funcType,funcName);
+}
+
+
+
+let spec = query(".special");
+let ham = query(".ham");
+let menu = query(".ham-menu");
+let cancle = query(".cancle");
+
+changePosition()
+listener(show,"click",ham);
+listener(hide,"click",cancle);
+listener(loadImages,"load",window);
+
+
+function loadImages(){
+    let special = query(".special");
+    special.src = "images/tourist1.svg";
+}
+
+
+
+function changePosition(){
+
+    setTimeout(()=>{
+        spec.classList.remove("anime");
+    },3000)
+    
+}
+
+function show(){
+    menu.style.top = "0%";
+}
+
+function hide(){
+    menu.style.top = "-230%";
+}
+
+
